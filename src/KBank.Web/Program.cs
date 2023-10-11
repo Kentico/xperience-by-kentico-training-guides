4f6d9bdf129f20d89e725f2c1c7c97a9438423d4
Matthew Sandstrom <MatthewS@kentico.com>
2023-10-11 12:59:10 -0400

    Add cross site tracking

--- src/KBank.Web/Program.cs
+++ src/KBank.Web/Program.cs
@@ -7,27 +7,43 @@ using Kentico.Web.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Kentico.Activities.Web.Mvc;
+using Kentico.OnlineMarketing.Web.Mvc;
+using Kentico.CrossSiteTracking.Web.Mvc;
+using CMS.Helpers;
+using KBank.Web.Helpers.Cookies;
+using System.Linq;
 
-var KenticoOrigins = "_kenticoOrigins";
+var KBankAllowSpecificOrigins = "_kBankAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: KenticoOrigins,
+    options.AddPolicy(name: KBankAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("https://download.kentico.com");
+            policy.WithOrigins("https://the-domain-of-your-external-site.com").WithHeaders("content-type").AllowCredentials();
         });
 });
-
 
 // Enable desired Kentico Xperience features
 builder.Services.AddKentico(features =>
 {
+    var cookieConsentMapping = CookieConsentHelper.GetCurrentMapping().Result;
+    features.UseCrossSiteTracking(
+        new CrossSiteTrackingOptions
+        {
+            ConsentSettings = new CrossSiteTrackingConsentOptions
+            {
+                ConsentName = cookieConsentMapping.MarketingConsentCodeName.FirstOrDefault(),
+                AgreeCookieLevel = CookieLevel.Visitor
+            }
+        });
+
     features.UsePageBuilder();
     features.UseActivityTracking();
     features.UsePageRouting();
+    
 });
 
 builder.Services.AddAuthentication();
@@ -55,6 +71,6 @@ app.UseAuthentication();
 
 app.Kentico().MapRoutes();
 
-app.UseCors(KenticoOrigins);
+app.UseCors(KBankAllowSpecificOrigins);
 
 app.Run();
