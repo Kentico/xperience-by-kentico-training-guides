2d5ec7fafd432a5ff14172ab8b9ec95a8384fb6c
Matthew Sandstrom <MatthewS@kentico.com>
2023-11-30 16:18:15 -0500

    Refactor asynchronous calls to improve consistency

--- src/TrainingGuides - External Site/activity.js
+++ src/TrainingGuides - External Site/activity.js
@@ -1,11 +1,8 @@
 //Fetches the codename of the marketing consent
 async function getConsentCodeName(){
-    let consentJson;
-    const res = await fetch("https://The-URL-of-your-Xperience-site.com/");
-
-    consentJson = await res.json();
-
-    return JSON.stringify(consentJson).replace(/"+/g, '');
+    return fetch("https://The-URL-of-your-Xperience-site.com/")
+        .then(response => response.json())
+        .then(consentJson => JSON.stringify(consentJson).replace(/"+/g, ''));
 }
 
 // Click handler that creates a consent agreement for the current contact
@@ -65,47 +62,48 @@ document.addEventListener('DOMContentLoaded', function () {
         onerror: t => console.log(t)
     });
 
-    getConsentCodeName().then((consentName) => {
-        // Retrieves and displays the consent text
-        kxt('consentdata', {
-            codeName: consentName,
-            languageName: 'en',
-            callback: consentData => {
-                document.getElementById('lblConsentText').innerHTML = consentData.shortText;
-            },
-            onerror: t => console.log(t)
-        });
+    getConsentCodeName()
+        .then((consentName) => {
+            // Retrieves and displays the consent text
+            kxt('consentdata', {
+                codeName: consentName,
+                languageName: 'en',
+                callback: consentData => {
+                    document.getElementById('lblConsentText').innerHTML = consentData.shortText;
+                },
+                onerror: t => console.log(t)
+            });
 
-        // Enables tracking if the current contact has agreed with the consent
-        kxt('consentcontactstatus', {
-            codeName: consentName,
-            callback: consentStatus => {
-                if (consentStatus.isAgreed) {
-                    kxt('updateconsent', {
-                        allow_tracking: true,
-                        allow_datainput: true
-                    });
-                }
-            },
-            onerror: t => console.log(t)
-        });
+            // Enables tracking if the current contact has agreed with the consent
+            kxt('consentcontactstatus', {
+                codeName: consentName,
+                callback: consentStatus => {
+                    if (consentStatus.isAgreed) {
+                        kxt('updateconsent', {
+                            allow_tracking: true,
+                            allow_datainput: true
+                        });
+                    }
+                },
+                onerror: t => console.log(t)
+            });
 
-        // Logs a page visit activity (if tracking is enabled for the current contact)
-        kxt('pagevisit', {
-            onerror: t => console.log(t)
-        });
+            // Logs a page visit activity (if tracking is enabled for the current contact)
+            kxt('pagevisit', {
+                onerror: t => console.log(t)
+            });
 
-        //Registers click event handlers for consent functions
-        const consentAgreeButton = document.getElementById("btnConsentAgree");
-        consentAgreeButton.addEventListener("click", function () {
-            trackingConsentAgree(consentName);  
-        });
+            //Registers click event handlers for consent functions
+            const consentAgreeButton = document.getElementById("btnConsentAgree");
+            consentAgreeButton.addEventListener("click", function () {
+                trackingConsentAgree(consentName);  
+            });
 
-        const consentRevokeButton = document.getElementById("btnConsentRevoke");
-        consentRevokeButton.addEventListener("click", function () {
-            trackingConsentRevoke(consentName);
+            const consentRevokeButton = document.getElementById("btnConsentRevoke");
+            consentRevokeButton.addEventListener("click", function () {
+                trackingConsentRevoke(consentName);
+            });
         });
-    });
 
     const links = document.getElementsByTagName("a");
     //Registers click event handlers for download and standard links
