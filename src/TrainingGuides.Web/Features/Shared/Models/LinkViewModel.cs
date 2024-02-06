a5e712b4db5c787b92b245bb59706cf33a4bb9e7
Matthew Sandstrom <MatthewS@kentico.com>
2024-02-06 15:31:30 -0500

    GH-22 :: Remove commented out code

--- src/TrainingGuides.Web/Features/Shared/Models/LinkViewModel.cs
+++ src/TrainingGuides.Web/Features/Shared/Models/LinkViewModel.cs
@@ -7,12 +7,4 @@ public class LinkViewModel
     public string? Page { get; set; }
     public string? LinkToExternal { get; set; }
     public bool OpenInNewTab { get; set; }
-
-    // Is the comment below left here on purpose for a concrete future need? If not, consider removing it.
-    //public static LinkViewModel GetViewModel(Cta cta) => new LinkViewModel()
-    //{
-    //    CTAText = cta.CtaCallToAction,
-    //    LinkTitleText = cta.CtaLinkTitleText,
-    //    LinkToExternal = cta.CtaLinkToExternal,
-    //};
 }
