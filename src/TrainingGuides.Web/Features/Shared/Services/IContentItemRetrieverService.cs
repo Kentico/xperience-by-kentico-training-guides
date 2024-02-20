8feb80573e1ffe4411ac1028efd6d3e58c8720dc
Matthew Sandstrom <MatthewS@kentico.com>
2024-02-20 12:32:25 -0500

    GH-32 :: fix IContentItemRetrieverService interce

--- src/TrainingGuides.Web/Features/Shared/Services/IContentItemRetrieverService.cs
+++ src/TrainingGuides.Web/Features/Shared/Services/IContentItemRetrieverService.cs
@@ -4,37 +4,34 @@ namespace TrainingGuides.Web.Features.Shared.Services;
 
 public interface IContentItemRetrieverService<T>
 {
-    public interface IContentItemRetrieverService<T>
-    {
-        public Task<T> RetrieveWebPageById(int webPageItemId,
-            string contentTypeName,
-            Func<IWebPageContentQueryDataContainer, T> resultSelector,
-            int depth = 1);
+    public Task<T> RetrieveWebPageById(int webPageItemId,
+        string contentTypeName,
+        Func<IWebPageContentQueryDataContainer, T> resultSelector,
+        int depth = 1);
 
-        public Task<T> RetrieveWebPageByGuid(Guid? webPageItemGuid,
-            string contentTypeName,
-            Func<IWebPageContentQueryDataContainer, T> resultSelector,
-            int depth = 1);
+    public Task<T> RetrieveWebPageByGuid(Guid? webPageItemGuid,
+        string contentTypeName,
+        Func<IWebPageContentQueryDataContainer, T> resultSelector,
+        int depth = 1);
 
-        public Task<IEnumerable<T>> RetrieveWebPageContentItems(string contentTypeName,
-            Func<ContentTypeQueryParameters, ContentTypeQueryParameters> queryFilter,
-            Func<IWebPageContentQueryDataContainer, T> resultSelector);
+    public Task<IEnumerable<T>> RetrieveWebPageContentItems(string contentTypeName,
+        Func<ContentTypeQueryParameters, ContentTypeQueryParameters> queryFilter,
+        Func<IWebPageContentQueryDataContainer, T> resultSelector);
 
-        public Task<IEnumerable<T>> RetrieveWebPageChildrenByPath(
-            string parentPageContentTypeName,
-            string path,
-            Func<IWebPageContentQueryDataContainer, T> resultSelector,
-            int depth = 1);
+    public Task<IEnumerable<T>> RetrieveWebPageChildrenByPath(
+        string parentPageContentTypeName,
+        string path,
+        Func<IWebPageContentQueryDataContainer, T> resultSelector,
+        int depth = 1);
 
-        public Task<T> RetrieveContentItemByGuid(
-            Guid contentItemGuid,
-            string contentTypeName,
-            Func<IContentQueryDataContainer, T> resultSelector,
-            int depth = 1);
+    public Task<T> RetrieveContentItemByGuid(
+        Guid contentItemGuid,
+        string contentTypeName,
+        Func<IContentQueryDataContainer, T> resultSelector,
+        int depth = 1);
 
-        public Task<IEnumerable<T>> RetrieveReusableContentItems(
-            string contentTypeName,
-            Func<ContentTypeQueryParameters, ContentTypeQueryParameters> queryFilter,
-            Func<IContentQueryDataContainer, T> resultSelector);
-    }
+    public Task<IEnumerable<T>> RetrieveReusableContentItems(
+        string contentTypeName,
+        Func<ContentTypeQueryParameters, ContentTypeQueryParameters> queryFilter,
+        Func<IContentQueryDataContainer, T> resultSelector);
 }
