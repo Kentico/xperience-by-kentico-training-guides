c40f50cbe020ee7b5c5c764e2b3eab6e57e31984
Matthew Sandstrom <MatthewS@kentico.com>
2024-02-23 12:44:05 -0500

    GH-36 :: decorate template properties, adjust color enum

--- src/TrainingGuides.Web/Features/Shared/OptionProviders/ColorScheme/ColorSchemeOption.cs
+++ src/TrainingGuides.Web/Features/Shared/OptionProviders/ColorScheme/ColorSchemeOption.cs
@@ -5,20 +5,26 @@ namespace TrainingGuides.Web.Features.Shared.OptionsProviders.ColorScheme;
 public enum ColorSchemeOption
 {
     [Description("Auto")]
-    Auto,
+    Auto = 0,
+
+    [Description("Transparent with light text")]
+    TransparentLight = 1,
+
+    [Description("Transparent with dark text")]
+    TransparentDark = 2,
 
     [Description("Dark 1")]
-    Dark1,
+    Dark1 = 3,
 
     [Description("Dark2")]
-    Dark2,
+    Dark2 = 4,
 
     [Description("Light 1")]
-    Light1,
+    Light1 = 5,
 
     [Description("Light 2")]
-    Light2,
+    Light2 = 6,
 
     [Description("Light 3")]
-    Light3,
+    Light3 = 7,
 }
--- src/TrainingGuides.Web/Features/Shared/Templates/GeneralPageTemplateProperties.cs
+++ src/TrainingGuides.Web/Features/Shared/Templates/GeneralPageTemplateProperties.cs
@@ -1,12 +1,36 @@
 ﻿using Kentico.PageBuilder.Web.Mvc.PageTemplates;
 using Kentico.Xperience.Admin.Base.FormAnnotations;
+using TrainingGuides.Web.Features.Shared.OptionProviders;
+using TrainingGuides.Web.Features.Shared.OptionProviders.ColumnLayout;
+using TrainingGuides.Web.Features.Shared.OptionProviders.CornerType;
+using TrainingGuides.Web.Features.Shared.OptionsProviders.ColorScheme;
 
 namespace TrainingGuides.Web.Features.Shared;
 
 public class GeneralPageTemplateProperties : IPageTemplateProperties
 {
+    [DropDownComponent(
+        Label = "Column layout",
+        ExplanationText = "Select the layout of the editable areas in the template.",
+        DataProviderType = typeof(DropdownEnumOptionProvider<ColumnLayoutOption>),
+        Order = 10
+    )]
     public string ColumnLayout { get; set; }
+
+    [DropDownComponent(
+        Label = "Color scheme",
+        ExplanationText = "Select the color scheme of the template.",
+        DataProviderType = typeof(DropdownEnumOptionProvider<ColorSchemeOption>),
+        Order = 20
+    )]
     public string ColorScheme { get; set; }
+
+    [DropDownComponent(
+        Label = "Corner type",
+        ExplanationText = "Select the corner type of the template.",
+        DataProviderType = typeof(DropdownEnumOptionProvider<CornerTypeOption>),
+        Order = 30
+    )]
     public string CornerType { get; set; }
 
 }
