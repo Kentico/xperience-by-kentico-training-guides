708c8f3e1c0443c31af755474f6cbd06e914c7d2
DominikaG2 <dominikag2@kentico.com>
2024-02-26 16:06:31 -0500

    GH-38 :: small KBank CTA button refactoring

--- src/TrainingGuides.Web/Features/LandingPages/Widgets/CallToAction/CallToActionWidgetProperties.cs
+++ src/TrainingGuides.Web/Features/LandingPages/Widgets/CallToAction/CallToActionWidgetProperties.cs
@@ -7,31 +7,57 @@ namespace TrainingGuides.Web.Features.LandingPages.Widgets.CallToAction;
 
 public class CallToActionWidgetProperties : IWidgetProperties
 {
-    [TextInputComponent(Order = 0, Label = "CTA button text", ExplanationText = "Add your call to action. Keep it under 30 characters.")]
+    [TextInputComponent(
+        Label = "CTA button text",
+        ExplanationText = "Add your call to action. Keep it under 30 characters.",
+        Order = 10
+    )]
     public string? Text { get; set; }
 
-    [RadioGroupComponent(Label = "Target content", ExplanationText = "Select what happens when a visitor clicks your CTA.", Options = "page;Page\ncontent;Content hub asset\nabsolute;Absolute URL", Order = 1)]
+    [RadioGroupComponent(
+        Label = "Target content",
+        ExplanationText = "Select what happens when a visitor clicks your CTA.",
+        Options = "page;Page\ncontent;Content hub asset\nabsolute;Absolute URL",
+        Order = 20)]
     public string Type { get; set; } = "page";
 
-    [UrlSelectorComponent(Label = "Target page", Order = 2, ExplanationText = "Select the page in the tree.")]
+    [UrlSelectorComponent(
+        Label = "Target page",
+        ExplanationText = "Select the page in the tree.",
+        Order = 30)]
     [VisibleIfEqualTo(nameof(Type), "page", StringComparison.OrdinalIgnoreCase)]
     public string? TargetPage { get; set; }
 
-    [ContentItemSelectorComponent(Asset.CONTENT_TYPE_NAME, Label = "Content hub asset", Order = 2, ExplanationText = "Select a file (image, video, PDF, etc. that opens when a visitor clicks your CTA.\r\n")]
+    [ContentItemSelectorComponent(
+        Asset.CONTENT_TYPE_NAME,
+        Label = "Content hub asset",
+        ExplanationText = "Select a file (image, video, PDF, etc. that opens when a visitor clicks your CTA.\r\n",
+        Order = 40)]
     [VisibleIfEqualTo(nameof(Type), "content", StringComparison.OrdinalIgnoreCase)]
     public IEnumerable<ContentItemReference>? ContentItem { get; set; }
 
-    [TextInputComponent(Order = 3, Label = "Absolute URL", ExplanationText = "Add a hyperlink to an external site, or use the product's URL + anchor tag # for referencing an anchor on the page, for example, \"https://your-doma.in/contact-us#form\"")]
+    [TextInputComponent(
+        Label = "Absolute URL",
+        ExplanationText = "Add a hyperlink to an external site, or use the product's URL + anchor tag # for referencing an anchor on the page, for example, \"https://your-doma.in/contact-us#form\"",
+        Order = 50)]
     [VisibleIfEqualTo(nameof(Type), "absolute", StringComparison.OrdinalIgnoreCase)]
     public string? AbsoluteUrl { get; set; }
 
-    [CheckBoxComponent(Order = 4, Label = "File download", ExplanationText = "Select if you want to log “File download activity” to the contact.")]
+    [CheckBoxComponent(
+        Label = "File download",
+        ExplanationText = "Select if you want to log “File download activity” to the contact.",
+        Order = 60)]
     [VisibleIfEqualTo(nameof(Type), "absolute", StringComparison.OrdinalIgnoreCase)]
     public bool IsDownload { get; set; } = false;
 
-    [TextInputComponent(Order = 5, Label = "CTA unique data identifier", ExplanationText = "Use this field to set an identifier of this widget instance. For example, it can come in handy for logging custom activities or distinguishing personalization variants.")]
+    [TextInputComponent(
+        Label = "CTA unique data identifier",
+        ExplanationText = "Use this field to set an identifier of this widget instance. For example, it can come in handy for logging custom activities or distinguishing personalization variants.",
+        Order = 70)]
     public string? Identifier { get; set; }
 
-    [CheckBoxComponent(Order = 6, Label = "Open in new tab")]
+    [CheckBoxComponent(
+        Label = "Open in new tab",
+        Order = 80)]
     public bool OpenInNewTab { get; set; }
 }
--- src/TrainingGuides.Web/Features/LandingPages/Widgets/CallToAction/CallToActionWidgetViewModel.cs
+++ src/TrainingGuides.Web/Features/LandingPages/Widgets/CallToAction/CallToActionWidgetViewModel.cs
@@ -1,10 +1,14 @@
+using TrainingGuides.Web.Features.Shared.Models;
+
 namespace TrainingGuides.Web.Features.LandingPages.Widgets.CallToAction;
 
-public class CallToActionWidgetViewModel
+public class CallToActionWidgetViewModel: WidgetViewModel
 {
     public string? Text { get; set; }
     public string? Url { get; set; }
     public bool OpenInNewTab { get; set; }
     public string? Identifier { get; set; }
     public bool IsDownload { get; set; }
+    public override bool IsMisconfigured => string.IsNullOrWhiteSpace(Text) || string.IsNullOrWhiteSpace(Url);
+
 }
