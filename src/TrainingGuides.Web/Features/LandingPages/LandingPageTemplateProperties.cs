b8c35f4355d524d45362ab3fc306fc8a5bdb8136
DominikaG2 <dominikag2@kentico.com>
2024-03-13 18:20:22 -0400

    GH-35 :: remove manual getter/setter - there will never be an invalid value with the dropdown control. - this state will be easier/cleaner to work from for the quick start guide that shows implementing enum mapping to the dropdown

--- src/TrainingGuides.Web/Features/LandingPages/LandingPageTemplateProperties.cs
+++ src/TrainingGuides.Web/Features/LandingPages/LandingPageTemplateProperties.cs
@@ -5,35 +5,18 @@ namespace TrainingGuides.Web.Features.LandingPages;
 
 public class LandingPageTemplateProperties : IPageTemplateProperties
 {
-    private const string DESCRIPTION = "Radio button group to select the type of HTML tag that wraps the home page message";
+    private const string DESCRIPTION = "Select the type of Html tag that wraps the home page message.";
 
     private const string OPTIONS =
-        "h1;Heading 1" + "\r\n" +
-        "h2;Heading 2" + "\r\n" +
-        "h3;Heading 3" + "\r\n" +
-        "h4;Heading 4" + "\r\n" +
-        "p;Paragraph" + "\r\n";
-
-    private string? messageType;
+        "H1;Heading 1" + "\n" +
+        "H2;Heading 2" + "\n" +
+        "H3;Heading 3" + "\n" +
+        "H4;Heading 4" + "\n" +
+        "P;Paragraph" + "\n";
 
     [DropDownComponent(
         Label = "Message tag type",
         Options = OPTIONS,
         ExplanationText = DESCRIPTION)]
-    public string MessageType
-    {
-        get => GetSafeTagText(messageType);
-        set => messageType = value;
-    }
-
-    //ensures only valid strings will be rendered as raw html
-    private string GetSafeTagText(string? messageType) => messageType switch
-    {
-        "h1" => "h1",
-        "h2" => "h2",
-        "h3" => "h3",
-        "h4" => "h4",
-        "p" => "p",
-        _ => "span"
-    };
+    public string MessageType { get; set; } = "H4";
 }
