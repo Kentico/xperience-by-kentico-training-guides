a8ba5c0c039ee73cc2ba32235704c42664cd5d31
Matthew Sandstrom <MatthewS@kentico.com>
2024-03-26 09:36:45 -0400

    GH-39 :: refactor tag helpers

--- src/TrainingGuides.Web/Features/Shared/Helpers/TagHelpers/ConfigureWidgetInstructionsTagHelper.cs
+++ src/TrainingGuides.Web/Features/Shared/Helpers/TagHelpers/ConfigureWidgetInstructionsTagHelper.cs
@@ -15,8 +15,7 @@ public class ConfigureWidgetInstructionsTagHelper : TagHelper
     private readonly IHttpContextAccessor accessor;
     public string? Message { get; set; }
 
-    private const string OPENING_TAG = "<p class=\"m-5\">";
-    private const string CLOSING_TAG = "</p>";
+    private const string P_TAG = "p";
     private const string INSTRUCTIONS_EDIT_MODE = "This widget needs some setup. Click the <strong>Configure widget</strong> gear icon in the top right to configure content and design for this widget.";
     private const string INSTRUCTIONS_PREVIEW_MODE = "This widget needs some setup. Switch to the <strong>Page Builder</strong> and <strong>Edit page</strong>.<br/>Then click the <strong>Configure widget</strong> gear icon in the top right to configure content and design for this widget.";
 
@@ -27,13 +26,14 @@ public class ConfigureWidgetInstructionsTagHelper : TagHelper
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
-        var httpContext = accessor.HttpContext;
+        output.TagName = P_TAG;
+        output.TagMode = TagMode.StartTagAndEndTag;
 
-        output.TagName = "";
+        var httpContext = accessor.HttpContext;
         string messageToShow = Message ?? (httpContext.Kentico().PageBuilder().EditMode
                 ? INSTRUCTIONS_EDIT_MODE
                 : INSTRUCTIONS_PREVIEW_MODE);
 
-        output.Content.SetHtmlContent(new HtmlString(OPENING_TAG + messageToShow + CLOSING_TAG));
+        output.Content.SetHtmlContent(new HtmlString(messageToShow));
     }
 }
