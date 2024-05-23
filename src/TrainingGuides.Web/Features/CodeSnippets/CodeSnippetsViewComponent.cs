63436695b6498c767891763200f3d3ad198b3057
Matthew Sandstrom <MatthewS@kentico.com>
2024-05-23 16:29:25 -0400

    GH-49 :: create means to include code snippets on live site

---
+++ src/TrainingGuides.Web/Features/CodeSnippets/CodeSnippetsViewComponent.cs
@@ -0,0 +1,46 @@
+using CMS.DataEngine;
+using CMS.Websites.Routing;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using TrainingGuides.ProjectSettings;
+
+namespace TrainingGuides.Web.Features.CodeSnippets;
+
+public class CodeSnippetsViewComponent : ViewComponent
+{
+    private readonly IInfoProvider<WebChannelSnippetInfo> webChannelSnippetInfoProvider;
+    private readonly IInfoProvider<WebChannelSettingsInfo> webChannelSettingsInfoProvider;
+    private readonly IWebsiteChannelContext websiteChannelContext;
+
+    public CodeSnippetsViewComponent(IInfoProvider<WebChannelSnippetInfo> webChannelSnippetInfoProvider, IInfoProvider<WebChannelSettingsInfo> webChannelSettingsInfoProvider, IWebsiteChannelContext websiteChannelContext)
+    {
+        this.webChannelSnippetInfoProvider = webChannelSnippetInfoProvider;
+        this.webChannelSettingsInfoProvider = webChannelSettingsInfoProvider;
+        this.websiteChannelContext = websiteChannelContext;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(CodeSnippetType codeSnippetType, bool addLabelComments = false)
+    {
+        string channel = websiteChannelContext.WebsiteChannelName;
+
+        var settings = await webChannelSettingsInfoProvider.Get()
+            .WhereContains(nameof(WebChannelSettingsInfo.WebChannelSettingsChannel), channel)
+            .GetEnumerableTypedResultAsync();
+
+        var setting = settings.FirstOrDefault();
+
+        var snippets = await webChannelSnippetInfoProvider.Get()
+            .WhereEquals(nameof(WebChannelSnippetInfo.WebChannelSnippetWebChannelSettingsId), setting?.WebChannelSettingsID ?? 0)
+            .WhereEquals(nameof(WebChannelSnippetInfo.WebChannelSnippetType), codeSnippetType.ToString())
+            .GetEnumerableTypedResultAsync();
+
+        var model = snippets.Select(snippet => new CodeSnippetViewModel
+        {
+            CodeSnippet = new HtmlString(snippet.WebChannelSnippetCode),
+            CodeSnippetType = snippet.WebChannelSnippetType,
+            CodeSnippetLabel = addLabelComments ?
+                snippet.WebChannelSnippetDisplayName : string.Empty
+        });
+        return View("~/Features/CodeSnippets/CodeSnippetsViewComponent.cshtml", model: model);
+    }
+}
