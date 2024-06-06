6e0e32a6c0cde85242e588c7cc81d33d40cfa3e2
Matthew Sandstrom <MatthewS@kentico.com>
2024-06-06 11:12:13 -0400

    GH-49 :: prevent edit page from messing up breadcrumbs of parent object with wrong name

--- src/TrainingGuides.Admin/Pages/ProjectSettings/SeoSettingsEdit.cs
+++ src/TrainingGuides.Admin/Pages/ProjectSettings/SeoSettingsEdit.cs
@@ -18,6 +18,12 @@ namespace TrainingGuides.Admin.ProjectSettings;
 public class SeoSettingsEdit : InfoEditPage<SeoSettingsInfo>
 {
     private readonly IInfoProvider<SeoSettingsInfo> seoSettingsInfoProvider;
+    private readonly IInfoProvider<WebChannelSettingsInfo> webChannelSettingsInfoProvider;
+
+    private string WebChannelSettingsDisplayName => webChannelSettingsInfoProvider.Get()
+        .WhereEquals(nameof(WebChannelSettingsInfo.WebChannelSettingsID), WebChannelSettingsId)
+        .FirstOrDefault()?
+        .WebChannelSettingsChannelDisplayName ?? "Web channel settings";
 
     [PageParameter(typeof(IntPageModelBinder))]
     public int WebChannelSettingsId { get; set; }
@@ -34,15 +40,29 @@ public class SeoSettingsEdit : InfoEditPage<SeoSettingsInfo>
         set { }
     }
 
-    public SeoSettingsEdit(IFormComponentMapper formComponentMapper, IFormDataBinder formDataBinder, IInfoProvider<SeoSettingsInfo> seoSettingsInfoProvider)
+    public SeoSettingsEdit(IFormComponentMapper formComponentMapper, IFormDataBinder formDataBinder, IInfoProvider<SeoSettingsInfo> seoSettingsInfoProvider, IInfoProvider<WebChannelSettingsInfo> webChannelSettingsInfoProvider)
              : base(formComponentMapper, formDataBinder)
     {
         this.seoSettingsInfoProvider = seoSettingsInfoProvider;
+        this.webChannelSettingsInfoProvider = webChannelSettingsInfoProvider;
     }
 
     public override Task ConfigurePage()
     {
         PageConfiguration.UIFormName = "seosettingsedit";
         return base.ConfigurePage();
+    }
+
+    protected async override Task<ICommandResponse> GetSubmitSuccessResponse(SeoSettingsInfo savedInfoObject, ICollection<IFormItem> items)
+    {
+        var result = new EditPageSuccessFormSubmissionResult()
+        {
+            Items = await items.OnlyVisible().GetClientProperties(),
+            ObjectDisplayName = WebChannelSettingsDisplayName,
+            ObjectId = WebChannelSettingsId,
+            RefetchAll = RefetchAll
+        };
+
+        return ResponseFrom(result).AddSuccessMessage(LocalizationService?.GetString("base.forms.saved"));
     }
 }
