f2ec9db747f811ca15920db25b160fd13cededa9
Matthew Sandstrom <MatthewS@kentico.com>
2024-08-29 14:28:17 -0400

    GH-69 :: refactor personalization condition type

--- src/TrainingGuides.Web/Features/Personalization/IsInContactGroupConditionType.cs
+++ src/TrainingGuides.Web/Features/Personalization/IsInContactGroupConditionType.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 using CMS.ContactManagement;
 using CMS.DataEngine;
 
@@ -10,10 +8,10 @@ using Kentico.Xperience.Admin.Base.FormAnnotations;
 using Kentico.Xperience.Admin.Base.Forms;
 
 [assembly: RegisterPersonalizationConditionType(
-	"TrainingGuides.Web.Features.Personalization",
-	typeof(IsInContactGroupConditionType),
-	"Is in contact group",
-	Description = "Evaluates if the current contact is in one of the contact groups.", IconClass = "icon-app-contact-groups", Hint = "Display to visitors who match at least one of the selected contact groups:")]
+    "TrainingGuides.Web.Features.Personalization",
+    typeof(IsInContactGroupConditionType),
+    "Is in contact group",
+    Description = "Evaluates if the current contact is in one of the contact groups.", IconClass = "icon-app-contact-groups", Hint = "Display to visitors who match at least one of the selected contact groups:")]
 
 namespace TrainingGuides.Web.Features.Personalization;
 
@@ -22,35 +20,35 @@ namespace TrainingGuides.Web.Features.Personalization;
 /// </summary>
 public class IsInContactGroupConditionType : ConditionType
 {
-	/// <summary>
-	/// Selected contact group code names.
-	/// </summary>
-	[ObjectSelectorComponent(PredefinedObjectType.CONTACTGROUP,
+    /// <summary>
+    /// Selected contact group code names.
+    /// </summary>
+    [ObjectSelectorComponent(PredefinedObjectType.CONTACTGROUP,
     Label = "Contact groups",
     Order = 0,
     MaximumItems = 0)]
-	public IEnumerable<ObjectRelatedItem> SelectedContactGroups { get; set; } = Enumerable.Empty<ObjectRelatedItem>();
+    public IEnumerable<ObjectRelatedItem> SelectedContactGroups { get; set; } = Enumerable.Empty<ObjectRelatedItem>();
 
 
-	/// <summary>
-	/// Evaluate condition type.
-	/// </summary>
-	/// <returns>Returns <c>true</c> if implemented condition is met.</returns>
-	public override bool Evaluate()
-	{
-		var contact = ContactManagementContext.GetCurrentContact();
+    /// <summary>
+    /// Evaluate condition type.
+    /// </summary>
+    /// <returns>Returns <c>true</c> if implemented condition is met.</returns>
+    public override bool Evaluate()
+    {
+        var contact = ContactManagementContext.GetCurrentContact();
 
-		if (contact == null)
-		{
-			return false;
-		}
+        if (contact == null)
+        {
+            return false;
+        }
 
-		if (SelectedContactGroups == null || !SelectedContactGroups.Any())
-		{
-			return contact.ContactGroups.Count == 0;
-		}
+        if (SelectedContactGroups == null || !SelectedContactGroups.Any())
+        {
+            return contact.ContactGroups.Count == 0;
+        }
 
-		return contact.IsInAnyContactGroup(SelectedContactGroups.Select(c => c.ObjectCodeName).ToArray());
-	}
+        return contact.IsInAnyContactGroup(SelectedContactGroups.Select(c => c.ObjectCodeName).ToArray());
+    }
 }
 
