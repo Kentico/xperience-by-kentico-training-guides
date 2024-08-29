39de8f57cc36906f4eb8efb5fde27b6e0fb6499e
DominikaG2 <dominikag2@kentico.com>
2024-08-29 16:26:23 -0400

    GH-72 :: update Code generation script: - include reusable field schemas - ensure the same namespace for RFS and other content types - use --with-provider-class False for Custom module classes (recommended) - extract repetitive code into a function

--- scripts/GenerateCodeFiles.ps1
+++ scripts/GenerateCodeFiles.ps1
@@ -9,53 +9,42 @@ Set-Location -Path $PSScriptRoot/../src/TrainingGuides.Web
 
 # https://docs.xperience.io/xp/developers-and-admins/development/content-retrieval/generate-code-files-for-xperience-objects
 
-#Reusable content types
-dotnet run --no-build -- --kxp-codegen --type "ReusableContentTypes" --location "../TrainingGuides.Entities/{type}/{name}"
+$contentTypesNamespace = "TrainingGuides"
 
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Reusable content type code generation failed."
-    $exitCode = 1
+function Write-Result-Get-Exit-Code {
+    param(
+        [string] $type
+    )    
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "$type code generation failed."
+        return 1;
+    }
+    else{
+        Write-Host "$type code generation succeeded." -ForegroundColor Green
+        return 0;
+    }
+    Write-Host
 }
-else{
-    Write-Host "Reusable content type code generation succeeded." -ForegroundColor Green
-}
-Write-Host
+
+#Reusable content types
+dotnet run --no-build -- --kxp-codegen --type "ReusableContentTypes" --namespace $contentTypesNamespace --location "../TrainingGuides.Entities/{type}/{name}"
+$exitCode = Write-Result-Get-Exit-Code "Reusable content type"
 
 #Page content types
-dotnet run --no-build -- --kxp-codegen --type "PageContentTypes" --location "../TrainingGuides.Entities/{type}/{name}" --skip-confirmation
+dotnet run --no-build -- --kxp-codegen --type "PageContentTypes" --namespace $contentTypesNamespace --location "../TrainingGuides.Entities/{type}/{name}" --skip-confirmation
+$exitCode = Write-Result-Get-Exit-Code "Page content type"
 
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Page content type code generation failed."
-    $exitCode = 1
-}
-else{
-    Write-Host "Page content type code generation succeeded." -ForegroundColor Green
-}
-Write-Host
+#Reusable field schemas
+dotnet run --no-build -- --kxp-codegen --type "ReusableFieldSchemas" --namespace $contentTypesNamespace --location "../TrainingGuides.Entities/{type}/{name}" --skip-confirmation
+$exitCode = Write-Result-Get-Exit-Code "Reusable field schema"
 
 #Custom module classes
-dotnet run --no-build -- --kxp-codegen --type "Classes" --location "../TrainingGuides.Entities/{type}/{name}" --skip-confirmation
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Class code generation failed."
-    $exitCode = 1
-}
-else{
-    Write-Host "Class code generation succeeded." -ForegroundColor Green
-}
-Write-Host
+dotnet run --no-build -- --kxp-codegen --type "Classes" --with-provider-class False --location "../TrainingGuides.Entities/{type}/{name}" --skip-confirmation
+$exitCode = Write-Result-Get-Exit-Code "Class"
 
 #Forms
 dotnet run --no-build -- --kxp-codegen --type "Forms" --location "../TrainingGuides.Entities/{type}/{name}" --skip-confirmation
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Form code generation failed."
-    $exitCode = 1
-}
-else{
-    Write-Host "Form code generation succeeded." -ForegroundColor Green
-}
-Write-Host
+$exitCode = Write-Result-Get-Exit-Code "Form"
 
 if ($exitCode -ne 0) {
     Set-Location -Path $originalLocation
