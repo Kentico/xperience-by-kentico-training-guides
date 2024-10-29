f8c357eac0fdf3da628b58ea4c745f86ebb6d33e
Dominika Glueck <146011583+dominikag2@users.noreply.github.com>
2024-10-29 08:17:17 -0400

    GH-74 :: add more Article unit tests - finished branch (#81)

---
+++ src/TrainingGuides.Web.Tests/Features/Articles/Widgets/ArticleList/ArticleListWidgetViewModelTests.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using TrainingGuides.Web.Features.Articles.Widgets.ArticleList;
+using TrainingGuides.Web.Features.Articles;
+
+namespace TrainingGuides.Web.Tests.Features.Articles.Widgets.ArticleList;
+
+public class ArticleListWidgetViewModelTests
+{
+    private readonly ArticleListWidgetViewModel emptyWidgetViewModel;
+
+    public ArticleListWidgetViewModelTests()
+    {
+        emptyWidgetViewModel = new();
+    }
+
+    [Fact]
+    public void WhenInitialized_ArticlesIsEmptyList() => Assert.Empty(emptyWidgetViewModel.Articles);
+
+    [Fact]
+    public void WhenInitialized_CtaTextIsEmpty() => Assert.Equal(string.Empty, emptyWidgetViewModel.CtaText);
+
+    [Fact]
+    public void IsMisconfigured_When_ArticlesEmpty_ReturnsTrue() => Assert.True(emptyWidgetViewModel.IsMisconfigured);
+
+    [Fact]
+    public void IsMisconfigured_When_ArticlesContainItems_ReturnsFalse()
+    {
+        var widgetViewModel = new ArticleListWidgetViewModel();
+        widgetViewModel.Articles.Add(new ArticlePageViewModel());
+
+        Assert.False(widgetViewModel.IsMisconfigured);
+    }
+}
