76a50cfb07c2a04e4485b0e9f8e19376ad63a575
DominikaG2 <dominikag2@kentico.com>
2025-03-27 14:23:31 -0400

    GH-111 :: fix tests

--- src/TrainingGuides.Web.Tests/Features/Articles/Services/ArticlePageServiceTests.cs
+++ src/TrainingGuides.Web.Tests/Features/Articles/Services/ArticlePageServiceTests.cs
@@ -4,6 +4,7 @@ using Moq;
 using TrainingGuides.Web.Features.Articles.Services;
 using TrainingGuides.Web.Features.Articles;
 using Microsoft.AspNetCore.Html;
+using Kentico.Content.Web.Mvc.Routing;
 
 namespace TrainingGuides.Web.Tests.Features.Articles.Services;
 
@@ -11,21 +12,29 @@ public class ArticlePageServiceTests
 {
     private readonly Mock<ArticlePageService> articlePageServiceMock;
     private readonly Mock<IWebPageUrlRetriever> webPageUrlRetrieverMock;
+    private readonly Mock<IPreferredLanguageRetriever> preferredLanguageRetrieverMock;
 
     private const string ARTICLE_TITLE = "Title";
     private const string ARTICLE_SUMMARY = "Summary";
     private const string ARTICLE_TEXT = "Text";
     private const string ARTICLE_URL = "test";
+    private const string ARTICLE_ABSOLUTE_URL = "https://test/test";
+    private const string LANGUAGE_EN = "en-US";
 
     private readonly ArticlePageViewModel referenceArticleViewModel;
 
     public ArticlePageServiceTests()
     {
         webPageUrlRetrieverMock = new Mock<IWebPageUrlRetriever>();
-        webPageUrlRetrieverMock.Setup(x => x.Retrieve(It.IsAny<ArticlePage>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new WebPageUrl(ARTICLE_URL));
+        webPageUrlRetrieverMock.Setup(x => x.Retrieve(It.IsAny<ArticlePage>(), LANGUAGE_EN, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new WebPageUrl(ARTICLE_URL, ARTICLE_ABSOLUTE_URL));
 
-        articlePageServiceMock = new Mock<ArticlePageService>(webPageUrlRetrieverMock.Object);
+        preferredLanguageRetrieverMock = new Mock<IPreferredLanguageRetriever>();
+        preferredLanguageRetrieverMock.Setup(x => x.Get()).Returns(LANGUAGE_EN);
+
+        articlePageServiceMock = new Mock<ArticlePageService>(
+            webPageUrlRetrieverMock.Object,
+            preferredLanguageRetrieverMock.Object);
 
         referenceArticleViewModel = new()
         {
