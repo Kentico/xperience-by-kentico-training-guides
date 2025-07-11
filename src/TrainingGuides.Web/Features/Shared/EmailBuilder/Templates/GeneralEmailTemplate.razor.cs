c791348e3db11f3329e044584f5d20e2243c0381
Matthew Sandstrom <MatthewS@kentico.com>
2025-07-11 14:59:28 -0400

    GH-124 :: Change general email template to render rows instead of columns

--- src/TrainingGuides.Web/Features/Shared/EmailBuilder/Templates/GeneralEmailTemplate.razor.cs
+++ src/TrainingGuides.Web/Features/Shared/EmailBuilder/Templates/GeneralEmailTemplate.razor.cs
@@ -3,7 +3,6 @@ using Kentico.Xperience.Mjml.StarterKit.Rcl;
 using Microsoft.AspNetCore.Components;
 using TrainingGuides.Web.Features.Shared.OptionProviders;
 using TrainingGuides.Web.Features.Shared.OptionProviders.ColorScheme;
-using TrainingGuides.Web.Features.Shared.OptionProviders.ColumnLayout;
 using TrainingGuides.Web.Features.Shared.OptionProviders.CornerStyle;
 using TrainingGuides.Web.Features.Shared.Services;
 
@@ -13,19 +12,15 @@ public partial class GeneralEmailTemplate : ComponentBase
 {
     public const string IDENTIFIER = "TrainingGuides.GeneralEmailTemplate";
 
+    public const string AREA_MAIN = "MainContent";
+
     private string Subject { get; set; } = string.Empty;
     private string EmailPreviewText { get; set; } = string.Empty;
-    private ColumnLayoutOption ColumnLayout => new DropdownEnumOptionProvider<ColumnLayoutOption>().Parse(Properties.ColumnLayout, ColumnLayoutOption.OneColumn);
     private CornerStyleOption CornerStyle => new DropdownEnumOptionProvider<CornerStyleOption>().Parse(Properties.CornerStyle, CornerStyleOption.Round);
-    private ColorSchemeOption MainColorScheme => new DropdownEnumOptionProvider<ColorSchemeOption>().Parse(Properties.MainColorScheme, ColorSchemeOption.Light1);
-    private ColorSchemeOption HeaderColorScheme => new DropdownEnumOptionProvider<ColorSchemeOption>().Parse(Properties.HeaderColorScheme, ColorSchemeOption.Light1);
-    private ColorSchemeOption Column1ColorScheme => new DropdownEnumOptionProvider<ColorSchemeOption>().Parse(Properties.Column1ColorScheme, ColorSchemeOption.Light1);
-    private ColorSchemeOption Column2ColorScheme => new DropdownEnumOptionProvider<ColorSchemeOption>().Parse(Properties.Column2ColorScheme, ColorSchemeOption.Light1);
-    private ColorSchemeOption Column3ColorScheme => new DropdownEnumOptionProvider<ColorSchemeOption>().Parse(Properties.Column3ColorScheme, ColorSchemeOption.Light1);
-    private ColorSchemeOption FooterColorScheme => new DropdownEnumOptionProvider<ColorSchemeOption>().Parse(Properties.FooterColorScheme, ColorSchemeOption.Light1);
+    private ColorSchemeOption MainColorScheme => new DropdownEnumOptionProvider<ColorSchemeOption>().Parse(Properties.MainColorScheme, ColorSchemeOption.Light2);
+    private List<string> RowIdentifiers { get; set; } = [];
     private List<string> MainCssClasses { get; set; } = [];
-    private List<string> HeaderCssClasses { get; set; } = [];
-    private List<string> FooterCssClasses { get; set; } = [];
+
     private string? cssContent;
     protected string CssContent
     {
@@ -56,10 +51,12 @@ public partial class GeneralEmailTemplate : ComponentBase
         MainCssClasses.AddRange(ComponentStyleEnumService.GetColorSchemeClasses(MainColorScheme));
         MainCssClasses.AddRange(cornerCssClasses);
 
-        HeaderCssClasses.AddRange(ComponentStyleEnumService.GetColorSchemeClasses(HeaderColorScheme));
-        HeaderCssClasses.AddRange(cornerCssClasses);
+        int numberOfRows = Math.Clamp(Properties.NumberOfRows, 1, 5);
+        for (int index = 0; index < numberOfRows; index++)
+        {
+            string rowIdentifier = index == 0 ? AREA_MAIN : $"Area_{index}";
+            RowIdentifiers.Add(rowIdentifier);
+        }
 
-        FooterCssClasses.AddRange(ComponentStyleEnumService.GetColorSchemeClasses(FooterColorScheme));
-        FooterCssClasses.AddRange(cornerCssClasses);
     }
 }
