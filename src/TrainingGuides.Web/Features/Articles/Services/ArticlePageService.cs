ae15c102746d9f3fc7b4db1e0868e0f78470f2b7
MASTER\DominikaG2 <dominikag2@kentico.com>
2025-07-29 13:54:24 -0400

    GH-125 :: remove unused code, fix tests

--- src/TrainingGuides.Web/Features/Articles/Services/ArticlePageService.cs
+++ src/TrainingGuides.Web/Features/Articles/Services/ArticlePageService.cs
@@ -1,4 +1,3 @@
-using Kentico.Content.Web.Mvc.Routing;
 using Microsoft.AspNetCore.Html;
 using TrainingGuides.Web.Features.Shared.Models;
 
@@ -6,14 +5,8 @@ namespace TrainingGuides.Web.Features.Articles.Services;
 
 public class ArticlePageService : IArticlePageService
 {
-    private readonly IWebPageUrlRetriever webPageUrlRetriever;
-    private readonly IPreferredLanguageRetriever preferredLanguageRetriever;
-    public ArticlePageService(IWebPageUrlRetriever webPageUrlRetriever,
-        IPreferredLanguageRetriever preferredLanguageRetriever)
-    {
-        this.webPageUrlRetriever = webPageUrlRetriever;
-        this.preferredLanguageRetriever = preferredLanguageRetriever;
-    }
+    public ArticlePageService()
+    { }
 
     /// <summary>
     /// Creates a new instance of <see cref="ArticlePageViewModel"/>, setting the properties using ArticlePage given as a parameter.
@@ -30,7 +23,7 @@ public class ArticlePageService : IArticlePageService
         var article = articlePage.ArticlePageContent.FirstOrDefault();
         var articleTeaserImage = article?.ArticleTeaser.FirstOrDefault();
 
-        string articleUrl = articlePage.GetUrl().RelativePath;
+        string articleUrl = GetArticlePageRelativeUrl(articlePage);
 
         return new ArticlePageViewModel
         {
@@ -42,4 +35,7 @@ public class ArticlePageService : IArticlePageService
             Url = articleUrl
         };
     }
+
+    public virtual string GetArticlePageRelativeUrl(ArticlePage articlePage) =>
+        articlePage?.GetUrl().RelativePath ?? string.Empty;
 }
