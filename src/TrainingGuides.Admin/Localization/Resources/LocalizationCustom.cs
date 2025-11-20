ae6ea5095fb13022eae2f98a081b66487b416374
MASTER\DominikaG2 <dominikag2@kentico.com>
2025-11-20 17:36:39 -0500

    GH-158 :: add Call to action widget localization

--- src/TrainingGuides.Admin/Localization/Resources/LocalizationCustom.cs
+++ src/TrainingGuides.Admin/Localization/Resources/LocalizationCustom.cs
@@ -5,6 +5,10 @@ using TrainingGuides.Admin.Localization.Resources;
 // Register resources for additional languages
 [assembly: RegisterLocalizationResource(typeof(LocalizationCustom), LocalizationTarget.Server, LocalizationConstants.SpanishMXCultureCode)]
 [assembly: RegisterLocalizationResource(typeof(LocalizationCustom), LocalizationTarget.Server, LocalizationConstants.FrenchCultureCode)]
+[assembly: RegisterLocalizationResource(typeof(LocalizationCustom), LocalizationTarget.Server, LocalizationConstants.EnglishUSCultureCode)]
+[assembly: RegisterLocalizationResource(typeof(LocalizationCustom), LocalizationTarget.Client, LocalizationConstants.SpanishMXCultureCode)]
+[assembly: RegisterLocalizationResource(typeof(LocalizationCustom), LocalizationTarget.Client, LocalizationConstants.FrenchCultureCode)]
+[assembly: RegisterLocalizationResource(typeof(LocalizationCustom), LocalizationTarget.Client, LocalizationConstants.EnglishUSCultureCode)]
 
 namespace TrainingGuides.Admin.Localization.Resources;
 
