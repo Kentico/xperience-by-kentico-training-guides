12f5186c80b09725351563dacbe20e2c210f49d6
Matthew Sandstrom <matthews@kentico.com>
2026-05-21 08:55:18 -0400

    GH-174 :: Update product stock module to async main branch (#179)

---
+++ src/TrainingGuides.Web/Features/Commerce/EventHandlers/ProductStockCreationHandlerModule.cs
@@ -0,0 +1,69 @@
+using CMS;
+using CMS.Base;
+using CMS.ContentEngine;
+using CMS.Core;
+using CMS.DataEngine;
+
+using TrainingGuides.Web.Commerce.EventHandlers;
+
+
+// Registers the custom module into the system
+[assembly: RegisterModule(typeof(ProductStockCreationHandlerModule))]
+
+namespace TrainingGuides.Web.Commerce.EventHandlers;
+
+public class ProductStockCreationHandlerModule : Module
+{
+    public ProductStockCreationHandlerModule() : base(nameof(ProductStockCreationHandlerModule)) { }
+
+    protected override void OnPreInit(ModulePreInitParameters parameters)
+    {
+        base.OnPreInit(parameters);
+
+        parameters.Services.AddSingleton<ProductStockCreationService>();
+        parameters.Services.AddEventHandler<AfterCreateContentItemEvent, ProductStockAfterCreateHandler>();
+        parameters.Services.AddEventHandler<BeforeUpdateDraftEvent, ProductStockBeforeUpdateDraftHandler>();
+        parameters.Services.AddEventHandler<AfterDeleteContentItemEvent, ProductStockAfterDeleteHandler>();
+    }
+}
+
+internal class ProductStockAfterCreateHandler(ProductStockCreationService service) : IAsyncEventHandler<AfterCreateContentItemEvent>
+{
+    public async Task HandleAsync(AfterCreateContentItemEvent asyncEvent, CancellationToken cancellationToken)
+    {
+        var data = asyncEvent.Data;
+
+        if (data.ID is null || !service.IsStockKeepingItem(data.ContentTypeName))
+            return;
+
+        data.ContentItemData.TryGetValue(nameof(IProductSkuSchema.ProductSkuSchemaSkuCode), out string skuCode);
+        await service.EnsureStockRecord(data.ID.Value, skuCode, cancellationToken);
+    }
+}
+
+internal class ProductStockBeforeUpdateDraftHandler(ProductStockCreationService service) : IAsyncEventHandler<BeforeUpdateDraftEvent>
+{
+    public async Task HandleAsync(BeforeUpdateDraftEvent asyncEvent, CancellationToken cancellationToken)
+    {
+        var data = asyncEvent.Data;
+
+        if (!service.IsStockKeepingItem(data.ContentTypeName))
+            return;
+
+        data.ContentItemData.TryGetValue(nameof(IProductSkuSchema.ProductSkuSchemaSkuCode), out string skuCode);
+        await service.EnsureStockRecord(data.ID, skuCode, cancellationToken);
+    }
+}
+
+internal class ProductStockAfterDeleteHandler(ProductStockCreationService service) : IAsyncEventHandler<AfterDeleteContentItemEvent>
+{
+    public async Task HandleAsync(AfterDeleteContentItemEvent asyncEvent, CancellationToken cancellationToken)
+    {
+        var data = asyncEvent.Data;
+
+        if (!service.IsStockKeepingItem(data.ContentTypeName))
+            return;
+
+        await service.DeleteStockRecords(data.ID, cancellationToken);
+    }
+}
